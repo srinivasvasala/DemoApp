e9d4468e825d419fba3003c9c817bf04b901a862
Srinivas Vasala <vasalasrinivas53@gmail.com>
2023-07-28 00:43:27 +0530

    First commit

---
+++ Demo App/src/DemoApp.java
@@ -0,0 +1,15 @@
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int a[]= {1,2,3,4,5,6,7};
+         for(int ele:a) {
+        	 System.out.println(ele);
+         }
+         
+         String name="Srinivas";
+         int count=name.toUpperCase().length();
+         System.out.println("lengh of The String is ::"+count);
+	}
+}
